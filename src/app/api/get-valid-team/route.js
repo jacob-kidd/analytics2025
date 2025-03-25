8702c1dccd476beb1489181b08a904b445531f9a
AanyaTolat <93947301+AanyaTolat@users.noreply.github.com>
2025-03-25 16:53:33 -0400

    njski

--- src/app/api/get-valid-team/route.js
+++ src/app/api/get-valid-team/route.js
@@ -18,7 +18,7 @@ export async function GET(request) {
     try {
         // Fetch match data from The Blue Alliance API
         const response = await fetch(
-            `https://www.thebluealliance.com/api/v3/event/2025nkski/matches/simple`,
+            `https://www.thebluealliance.com/api/v3/event/2025njski/matches/simple`,
             {
                 headers: {
                     "X-TBA-Auth-Key": process.env.TBA_AUTH_KEY,
